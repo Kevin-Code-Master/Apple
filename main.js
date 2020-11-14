81f5afea6ddcc9a7a1b51ff078a744d0a14cd03a
koluochochieng <koluoch28@gmail.com>
2020-11-14 17:31:32 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Designing the input field for search
+
+function focusIn(){
+	document.getElementById("search").style.width = "130px";
+}
+
+function focusOut(){
+	document.getElementById("search").style.width = "95px";
+}
